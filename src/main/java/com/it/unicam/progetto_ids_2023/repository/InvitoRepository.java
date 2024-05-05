891ea82861fe6ff6a3bed924678f15ead6bc65f9
Luca Santarelli <luca01.santarelli@studenti.unicam.it>
2024-05-05 17:55:05 +0200

    Sistemato un bug inviti

--- src/main/java/com/it/unicam/progetto_ids_2023/repository/InvitoRepository.java
+++ src/main/java/com/it/unicam/progetto_ids_2023/repository/InvitoRepository.java
@@ -4,11 +4,14 @@ import com.it.unicam.progetto_ids_2023.model.contenuto.Contest;
 import com.it.unicam.progetto_ids_2023.model.contenuto.Invito;
 import com.it.unicam.progetto_ids_2023.model.utente.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface InvitoRepository extends JpaRepository<Invito, Long> {
-  Optional<Utente> findByUtenteId(Long utenteId);
+  @Query("SELECT i.utente FROM Invito i JOIN i.utente u WHERE u.id = :id")
+
+  Optional<Utente> findByUtenteId(Long id);
 
 
 
